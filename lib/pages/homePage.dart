74445d293a21ca6de1bd65bc0dd3a6315a53538b
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2022-06-07 11:32:49 +0700

    Lecture_29_Zooming_to_Users_CurrentLocation

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -11,15 +12,24 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Completer<GoogleMapController> _controller = Completer();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    _controller.complete(controller);
+    final position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(position.latitude, position.longitude), zoom: 14)));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(children: <Widget>[
         GoogleMap(
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          },
+          myLocationEnabled: true,
+          onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(target: LatLng(45.521563, -122.677433), zoom: 14),
         )
       ]),
