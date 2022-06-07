6c0347a5b2d31e164751347cb70379864c1016fd
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2022-06-07 14:20:47 +0700

    Lecture_33_ImplementingandMappingJSON_to_Models

--- lib/services/webservice.dart
+++ lib/services/webservice.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:place_finder/models/place.dart';
 import 'package:place_finder/utils/urlHelper.dart';
 
 class Webservice {
-  void fetchPlacesByKeywordAndPosition(String keyword, double latitude, double longitude) async {
+  Future<List<Place>> fetchPlacesByKeywordAndPosition(String keyword, double latitude, double longitude) async {
     final url = UrlHelper.urlForPlaceKeywordAndLocation(keyword, latitude, longitude);
 
     final response = await http.get(url);
@@ -12,6 +13,9 @@ class Webservice {
     if (response.statusCode == 200) {
       final jsonResponse = jsonDecode(response.body);
       final Iterable results = jsonResponse["results"];
+      return results.map((place) => Place.fromJson(place)).toList();
+    } else {
+      throw Exception("Unable to perform request!");
     }
   }
 }
