701a9efc22c92e1ba233b6949975f3a2a3b157bf
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2022-06-08 13:27:16 +0700

    Lecture_34_ImplementingPlaceListViewModel_and_PlaceViewModel

---
+++ lib/viewmodels/placeListViewModel.dart
@@ -0,0 +1,14 @@
+import 'dart:core';
+import 'package:flutter/material.dart';
+import 'package:place_finder/services/webservice.dart';
+import 'package:place_finder/viewmodels/placeViewModel.dart';
+
+class PlaceListViewModel extends ChangeNotifier {
+  var places = <PlaceViewModel>[];
+
+  Future<void> fetchPlacesByKeywordAndPosition(String keyword, double latitude, double longitude) async {
+    final results = await Webservice().fetchPlacesByKeywordAndPosition(keyword, latitude, longitude);
+    this.places = results.map((place) => PlaceViewModel(place)).toList();
+    notifyListeners();
+  }
+}
